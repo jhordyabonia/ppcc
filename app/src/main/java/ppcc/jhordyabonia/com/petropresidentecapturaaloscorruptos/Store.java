cb351c2aa8ff80da8aa95a68fb8cb1bb4c1af803
jhordy abonia <jhordyabonia@bitbucket.org>
2018-05-13 00:42:34 -0500

    Nombres correctos asignados a las imagenes

--- app/src/main/java/ppcc/jhordyabonia/com/petropresidentecapturaaloscorruptos/Store.java
+++ app/src/main/java/ppcc/jhordyabonia/com/petropresidentecapturaaloscorruptos/Store.java
@@ -21,54 +21,54 @@ public class Store
 		{
 			store=new JSONArray();
 			JSONObject obj0= new JSONObject();
-			obj0.put(_NAME,"Vivian Morales");
+			obj0.put(_NAME,"Cesar Mauricio Velásquez");
 			obj0.put(_VELOCIDAD,4000);
 			obj0.put(_IMG,R.mipmap.c0);
 			obj0.put(_LIVES,3);
 			store.put(obj0);
 
 			JSONObject obj1= new JSONObject();
-			obj1.put(_NAME,"Ivan Duque");
+			obj1.put(_NAME,"José Obdulio Gaviria");
 			obj1.put(_VELOCIDAD,3500);
 			obj1.put(_IMG,R.mipmap.c2);
 			obj1.put(_LIVES,3);
 			store.put(obj1);
 
 			JSONObject obj2= new JSONObject();
-			obj2.put(_NAME,"Ordoñez");
+			obj2.put(_NAME,"Edmundo del Castillo");
 			obj2.put(_VELOCIDAD,3000);
 			obj2.put(_IMG,R.mipmap.c3);
 			obj2.put(_LIVES,3);
 			store.put(obj2);
 			JSONObject obj3= new JSONObject();
-			obj3.put(_NAME,"Vivian Morales");
+			obj3.put(_NAME,"Oscar Ivan Zuluaga");
 			obj3.put(_VELOCIDAD,2500);
 			obj3.put(_IMG,R.mipmap.c4);
 			obj3.put(_LIVES,2);
 			store.put(obj3);
 
 			JSONObject obj4= new JSONObject();
-			obj4.put(_NAME,"Andres Felipe Arias");
+			obj4.put(_NAME,"Luis Alfonso Hoyos");
 			obj4.put(_VELOCIDAD,2000);
 			obj4.put(_IMG,R.mipmap.c5);
 			obj4.put(_LIVES,2);
 			store.put(obj4);
 
 			JSONObject obj5= new JSONObject();
-			obj5.put(_NAME,"ELN");
+			obj5.put(_NAME,"Luis Carlos Restrepo");
 			obj5.put(_VELOCIDAD,1500);
 			obj5.put(_IMG,R.mipmap.c6);
 			obj5.put(_LIVES,3);
 			store.put(obj5);
 			JSONObject obj6= new JSONObject();
-			obj6.put(_NAME,"Criminalidad general");
+			obj6.put(_NAME,"Favio Buitrago");
 			obj6.put(_VELOCIDAD,1000);
 			obj6.put(_IMG,R.mipmap.c7);
 			obj6.put(_LIVES,4);
 			store.put(obj6);
 
 			JSONObject obj7= new JSONObject();
-			obj7.put(_NAME,"Dicidencias de las Farc");
+			obj7.put(_NAME,"Luis Guillermo Giraldo");
 			obj7.put(_VELOCIDAD,500);
 			obj7.put(_IMG,R.mipmap.c8);
 			obj7.put(_LIVES,4);
